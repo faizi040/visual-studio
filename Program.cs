bc48257963303b5a26d48ee1185e767f75751395
M.Faiz <rasoolf796@gmail.com>
2021-03-17 12:15:04 +0500

    first csharp program

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace my_local_repository
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
